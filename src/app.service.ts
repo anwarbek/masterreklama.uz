0d3de8169bb1be26eb0129e3b5d00ed4f35b2b5a
Anvarbek <arsbekyaw@gmail.com>
2025-11-21 18:37:35 +0500

    deploy1

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Masterreklamaga xush keliubsiz';
+    return 'Hello World! Masterreklamaga xush keliubsiz Uyga vazifa deploy bajarildi';
   }
 }
